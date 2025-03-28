2e7fac211bcc137bc3607e1c393be9017668b93f
rajshukla-a2g <rajshukla16052@gmail.com>
2025-03-28 17:03:47 +0530

    hello word commit

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 app.use("/api/jobs", jobRouter);
 
-app.get("/", (req, res) => res.send("Hello World!"));
+// app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(process.env.PORT || port, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`)
 );
