3fc77936006b18654607daf1c63404b5fefaccbd
rajshukla-a2g <rajshukla16052@gmail.com>
2025-04-03 11:49:50 +0530

    fetch job by id corrected

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -45,71 +45,70 @@ module.exports = {
   },
 
   getJob: async (req, res) => {
-    const job = req.params.id;
+    const jobId = req.params.id;
+    console.log(jobId);
     try {
-      const job = await Job.findById(
-        { _id: jobId },
-        { createdAt: 0, updatedAt: 0 }
-      );
-      res.status(200).json({
-        status: true,
-        message: "Job fetched successfully",
-        data: job,
-      });
-    } catch (err) {
-      res.status(500).json(err);
+        const job = await Job.findById({ _id: jobId }, { createdAt: 0, updatedAt: 0, __V: 0 });
+        res.status(200).json(job);
+    } catch (error) {
+        res.status(500).json(error);
     }
-  },
+},
+
 
 
   getAllJobs: async (req, res) => {
     const recent = req.query.new;
 
     try {
-        // let job = await Job.findById({},{createdAt:0, updatedAt:0, __V:0}).sort({createdAt:-1}).limit(2) 
-        let jobs;
-        if (recent) {
-            jobs = await Job.find({}, { createdAt: 0, updatedAt: 0, __V: 0 }).sort({ createdAt: -1 }).limit(2)
-        } else {
-            jobs = await Job.find({}, { createdAt: 0, updatedAt: 0, __V: 0 })
-        }
-        res.status(200).json(jobs)
+      // let job = await Job.findById({},{createdAt:0, updatedAt:0, __V:0}).sort({createdAt:-1}).limit(2)
+      let jobs;
+      if (recent) {
+        jobs = await Job.find({}, { createdAt: 0, updatedAt: 0, __V: 0 })
+          .sort({ createdAt: -1 })
+          .limit(2);
+      } else {
+        jobs = await Job.find({}, { createdAt: 0, updatedAt: 0, __V: 0 });
+      }
+      res.status(200).json(jobs);
     } catch (error) {
-        res.status(500)
+      res.status(500);
     }
-},
+  },
 
-searchJobs: async(req, res) => {
-        try {
-           const results = await Job.aggregate([
-            {
-              $search: {
-                index: "jobsearch",
-                text: {
-                  query: req.params.key,
-                  path: {
-                    wildcard: "*"
-                  }
-                }
-              }
-            }
-          ])
-          res.status(200).json(results)
-        } catch(error) {
-            res.status(500).json(error)
-        }
-},
+  searchJobs: async (req, res) => {
+    try {
+      const results = await Job.aggregate([
+        {
+          $search: {
+            index: "jobsearch",
+            text: {
+              query: req.params.key,
+              path: {
+                wildcard: "*",
+              },
+            },
+          },
+        },
+      ]);
+      res.status(200).json(results);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
 
-getAgentJobs: async(req, res)=> {
+  getAgentJobs: async (req, res) => {
     const uid = req.params.uid;
     try {
-        const agentJobs = await Job.find({
-            agentId:uid
-        }, { __v: 0, createdAt: 0, updatedAt: 0}).sort({createdAt: -1})
-        res.status(200).json(agentJobs)
-    } catch(error) {
-        res.status(500).json({error: error.message})
+      const agentJobs = await Job.find(
+        {
+          agentId: uid,
+        },
+        { __v: 0, createdAt: 0, updatedAt: 0 }
+      ).sort({ createdAt: -1 });
+      res.status(200).json(agentJobs);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
     }
-}
-
+  },
 };
